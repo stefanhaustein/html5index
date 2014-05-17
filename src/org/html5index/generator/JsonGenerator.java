51fb356d773b618970a7ba9c38557b5967add34c
Stefan Haustein <stefan.haustein@gmail.com>
2014-05-17 18:47:47 +0200

    Better JSON Schema compatibility. Generate only two files (ecma only and everything)

--- src/org/html5index/generator/JsonGenerator.java
+++ src/org/html5index/generator/JsonGenerator.java
@@ -6,10 +6,15 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.Writer;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
 
 import org.html5index.docscan.DefaultModelReader;
 import org.html5index.model.Library;
 import org.html5index.model.Model;
+import org.html5index.model.Operation;
+import org.html5index.model.Parameter;
 import org.html5index.model.Property;
 import org.html5index.model.Type;
 
@@ -22,9 +27,56 @@ public class JsonGenerator implements Runnable {
 		this.model = model;
 	}
 
-	String jsonType(Type t) {
-		// Adjust similar to jsdocType in JsdocGenerator
-		return t.toString();
+	String getQualifiedName(Type type) {
+		// TODO: Package name?!
+		return type.getName();
+	}
+	
+	String getRef(Type type) {
+		return "#descriptions/" + getQualifiedName(type);
+	}
+	
+	void writeType(JsonWriter out, Type type) {
+		if (type == null) {
+			return;
+		}
+		String name = type.getName();
+		
+		// This should really be a a wrapper in the representation (perhaps (type|null) as in Jsdoc?)
+		boolean optional = name.endsWith("?");
+		if (optional) {
+			name = name.substring(0, name.length() - 1);
+		}
+		
+		if (name.equals("any")) {
+			out.writeString("type", "object");
+			return;
+		} 
+		if (name.equals("DOMString")) {
+			out.writeString("type", "string");
+			return;
+		}
+
+		switch(type.getKind()) {
+		case PRIMITIVE:
+			out.writeString("type", (Arrays.binarySearch(JsdocGenerator.NUMBER_TYPES, name) < 0) ? name : "number");
+			break;
+		case UNION: 
+			out.writeString("type", "object");
+			out.openArray("oneOf");
+			for (Type t: type.getTypes()) {
+				out.openObject();
+				writeType(out, t);
+				out.closeObject();
+			}
+			out.closeArray();
+			break;
+		default:
+			out.writeString("type", "object");
+			if (!name.equals("Object")) {
+				out.writeString("$ref", getRef(type));
+			}
+		}
 	}
 	
 	public void run() {
@@ -39,32 +91,41 @@ public class JsonGenerator implements Runnable {
 			indexWriter.println("<p><em>Experimental</em> HTML5 API JSON export</p>");
 			indexWriter.println("<body>");
 			indexWriter.println("<ul>");
-		      
-			for (Library l: model.getLibraries()) {
-				generateLibrary(l, indexWriter);
-			}
-		      
+			indexWriter.println("<li><a href='ECMAScript.json'>ECMAScript only</a> (use this for experiments and testing)</li>");
+			indexWriter.println("<li><a href='HTML5.json'>HTML 5</a></li>");
 			indexWriter.println("</ul>");
+			indexWriter.println("<p>Please copy the files if you plan to work with them -- ");
+			indexWriter.println("hot-linking will probably overload this server.</p>");
 			indexWriter.println("</body>");
 			indexWriter.println("</html>");
 			indexWriter.close();
+			
+			generateModel(true);
+			generateModel(false);
+			
 		} catch(Exception e) {
 			throw new RuntimeException(e);
 		}	
 	}
 
-	public void generateLibrary(Library library, PrintWriter indexWriter) throws IOException {
-		String name = library.getName() + ".json";
-		indexWriter.println("<li><a href='" + name + "'>" + name + "</a></li>");  
+	public void generateModel(boolean ecmaOnly) throws IOException {
+		String name = ecmaOnly ? "ECMAScript" : "HTML5";
+		String fileName = name + ".json";
 		
-		File file = new File(root, name);
+		File file = new File(root, fileName);
+		
 		JsonWriter out = new JsonWriter(new PrintWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8")));
-
+		
 		out.writeString("name", name);
+		out.writeString("description", "Generated " + new Date() + (ecmaOnly ? "" : " from the HTML5 specifications") + " by the html5index.org generator.");
 		out.openObject("definitions");
 		
-		for (Type type: library.getTypes()) {
-			generateType(out, type);
+		for (Library library: model.getLibraries()) {
+			if (!ecmaOnly || library.getName().equals("ECMAScript")) {
+				for (Type type: library.getTypes()) {
+					generateType(out, type);
+				}
+			}
 		}
 		 
 		out.closeObject(); // definitions
@@ -73,19 +134,79 @@ public class JsonGenerator implements Runnable {
 
 	void generateProperty(JsonWriter out, Property property) {
 		out.openObject(property.getName());
-		out.writeString("name", property.getName());
-		out.writeString("type", jsonType(property.getType()));
+		writeType(out, property.getType());
+		out.closeObject();
+	}
+
+	void generateParameterList(JsonWriter out, Operation operation) {
+		out.openArray("parameter");
+		for (Parameter p: operation.getParameters()) {
+			out.openObject();
+			out.writeString("name", p.getName());
+			out.writeBoolean("optional", p.hasModifier(Parameter.OPTIONAL));
+			writeType(out, p.getType());
+			out.closeObject();
+		}
+		out.closeArray();
+	}
+
+	void generateConstructor(JsonWriter out, Operation operation) {
+		out.openObject();
+		generateParameterList(out, operation);
+		out.closeObject();
+	}
+
+	void generateOperation(JsonWriter out, Operation operation) {
+		out.openObject(operation.getName());
+		out.writeBoolean("static", operation.hasModifier(Operation.STATIC));
+		writeType(out, operation.getType());
+		generateParameterList(out, operation);
 		out.closeObject();
 	}
 	
 	void generateInterface(JsonWriter out, Type type) {
-		out.openObject(type.getName());
-		out.writeString("type", "Object");
-		out.openObject("properties");
-		for (Property p: type.getOwnProperties()) {
-			generateProperty(out, p);
+		out.openObject(getQualifiedName(type));
+		out.writeString("type", "object");
+		out.writeString("library", type.getLibrary().getName());
+		if (type.getSuperType() != null) {
+			out.writeString("extends", getRef(type.getSuperType()));
 		}
-		out.closeObject();
+		Collection<Type> interfaces = type.getTypes();
+		if (interfaces.size() > 0) {
+			out.openArray("implements");
+			for (Type i: interfaces) {
+				out.writeString(getRef(i));
+			}
+			out.closeArray();
+		}
+		
+		Collection<Property> properties = type.getOwnAndInterfaceProperties();
+		if (properties.size() > 0) {
+			out.openObject("properties");
+			for (Property p: properties) {
+				generateProperty(out, p);
+			}
+			out.closeObject();
+		}
+		
+		Collection<Operation> constructors = type.getConstructors();
+		if (constructors.size() > 0) {
+			out.openArray("constructors");
+			for (Operation o: constructors) {
+				generateConstructor(out, o);
+			}
+			out.closeArray();
+		}
+		
+		Collection<Operation> operations = type.getOwnAndInterfaceOperations();
+		if (operations.size() > 0) {
+			out.openObject("operations");
+			for (Operation o: operations) {
+				generateOperation(out, o);
+			}
+			out.closeObject();
+		}
+		
 		out.closeObject();
 	}
 	
--- src/org/html5index/generator/JsonWriter.java
+++ src/org/html5index/generator/JsonWriter.java
@@ -4,29 +4,54 @@ import java.io.PrintWriter;
 
 public class JsonWriter {
 	PrintWriter out;
-	String indent = "  ";
+	String indent = " ";
 	
 	JsonWriter(PrintWriter printWriter) {
 		this.out = printWriter;
 		out.println("{");
 	}
 	
+	void openObject() {
+		out.println(indent + "{");
+		indent += " ";
+	}
+	
 	void openObject(String name) {
 		out.println(indent + "'" + name + "': {");
-		indent = indent + "  ";
+		indent += " ";
 	}
 	
 	void closeObject() {
-		indent = indent.substring(0, indent.length() - 2);
+		indent = indent.substring(0, indent.length() - 1);
 		out.println(indent + "},");
 	}
 	
+	void writeString(String value) {
+		out.println(indent + "'" + value + "',");
+	}
+
 	void writeString(String name, String value) {
 		out.println(indent + "'" + name + "': '" + value + "',");
+	}
+	
+	void writeBoolean(String name, boolean value) {
+		if (value) {
+			out.println(indent + "'" + name + "': true,");
+		}
 	}
 	
 	void close() {
 		out.println("}");
 		out.close();
+	}
+
+	public void openArray(String name) {
+		out.println(indent + "'" + name + "' : [");
+		indent += " ";
+	}
+	
+	public void closeArray() {
+		indent = indent.substring(0, indent.length() - 1);
+		out.println(indent + "],");
 	}
 }
